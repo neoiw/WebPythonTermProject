7535bb9a6c6f13302cfd31f3c5cb9d8095174eb8
Neo <45356936+ohmaytt@users.noreply.github.com>
2022-06-01 16:22:24 +0900

    commit

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import os
+import csv
 
 def grab():
     user_token = input("Your Discord user token: ")
     channel_id = input("Discord channel ID: ")
-    os.system('cmd /c "dotnet .\\DiscordChatExporter.CLI\\DiscordChatExporter.Cli.dll export -t "{0}" -c {1} -f "PlainText" -o "output.txt"'.format(user_token,channel_id))
+    os.system('cmd /c "dotnet .\\DiscordChatExporter.CLI\\DiscordChatExporter.Cli.dll export -t "{0}" -c {1} -f "CSV" -o "output.csv"'.format(user_token,channel_id))
 
 
 def analyze():
